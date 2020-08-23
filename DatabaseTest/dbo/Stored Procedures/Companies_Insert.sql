71874069df147f33a56ecdea78e7f48877128ed3
MichalKolarzyk <michalkolarzyk@gmail.com>
2020-08-23 12:55:27 +0200

    Add publish location

--- DatabaseTest/dbo/Stored Procedures/Companies_Insert.sql
+++ DatabaseTest/dbo/Stored Procedures/Companies_Insert.sql
@@ -1,13 +1,10 @@
 ﻿
 
 CREATE PROCEDURE [dbo].[Companies_Insert]
-	-- Add the parameters for the stored procedure here
 	@ID varchar(20),
 	@FullName varchar(50)
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 	
 	IF Exists (Select ID From Companies WHERE ID=@ID)
